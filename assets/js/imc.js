420507ae44251efb276b8f9adb80c5bb2db0b7b2
QuiraDon <arthur_gabriel2.0@outlook.com>
2023-12-05 16:59:00 -0300

    Add files via upload

--- assets/js/imc.js
+++ assets/js/imc.js
@@ -1,113 +1,176 @@
-//quando o botão de submit do formulario "calc" for pressionado execute um alert com a mensagem "teste"
-document.getElementById("calc").addEventListener("submit", function (e) {
+$('#calcular').click(function(e) {
     e.preventDefault();
-
-
-
-
-    var calc_result = calc();
-    imc = calc_result[0];
-    classificacao = calc_result[1];
-    //modifique o texto do item de id resultClasifica para teste
-    document.getElementById("resultClasifica").innerHTML = classificacao;
-    document.getElementById("resultIMC").innerHTML = `seu IMC é ${imc}`;
-    $("#result").modal("show");
+    if(verifyInputs()){
+        $('#loading').show();
+        $('#result_values_div').hide();
+        $('#resultClasifica').text(ImcClasify());
+        $('#resultIMC').text('IMC: ' + getIMC());
+        $('#resultValue').text(getGET() + ' Kcal');
+        $('#loading').hide();
+        $('#result_values_div').show();
+        $('#result').modal('show');
+    }
 });
 
 
-//função que calcula o IMC
-function calc() {
-    var idade = document.getElementById("idade").value;
-    var peso = document.getElementById("peso").value;1
-    var altura = document.getElementById("altura").value;
-
-    //existe dois labels chamados sex1 e sex2 
-    //se o sex1 estiver checado o genero é masculino
-    //se o sex2 estiver checado o genero é feminino
-    var sex1 = document.getElementById("sex1")
-    if (sex1.checked) {
-        var genero = "feminino";
-    } else {
-        var genero = "masculino"
+function verifyInputs(){
+    //caso algum campo esteja vazio ou com um valor indevido, retorne um alerta
+    if(getIdade() == '' || getIdade() < 0 || getIdade() > 120){
+        alert('Idade invalida');
+        return false;
     }
-    //deixe altura em função de metros
-    altura = altura / 100;
-    //calcule o IMC e Classifique-o baseado em genero use os dados da Organização Mundial da saude para classificar
-    //altura estrá em centrimentros e peso em kilogramas
-    var imc = peso / (altura * altura);
-    var classificacao = "";
-    if (genero == "masculino") {
-        if (imc < 20.7) {
-            classificacao = "Abaixo do peso";
-        } else if (imc < 26.4) {
-            classificacao = "Peso normal";
-        } else if (imc < 27.8) {
-            classificacao = "Sobrepeso";
-        } else if (imc < 31.1) {
-            classificacao = "Obesidade grau 1";
-        } else {
-            classificacao = "Obesiade grau 2";
-        }
-    } else { 
-        if (imc < 19.1) {
-            classificacao = "Abaixo do peso";
-        } else if (imc < 25.8) {
-            classificacao = "Peso normal";
-        } else if (imc < 27.3) {
-            classificacao = "Sobre peso";
-        } else if (imc < 32.3) {
-            classificacao = "Obesidade grau 1";
-        } else {
-            classificacao = "Obesidade grau 2";
-        }
+    if(getAltura() == '' || getAltura() < 0){
+        alert('Altura invalida');
+        return false;
     }
-
-    //calcular valor nutricional
-
-    var vpeso = 0;
-    var valtura = 0;
-    var vidade = 0;
-    var vresultado = 0;
-    var vnenhuma = 0;   
-    var vmoderada = 0;      
-    var vintensa = 0;
-    var idade = document.getElementById("idade").value;
-    var peso = document.getElementById("peso").value;1
-    var altura = document.getElementById("altura").value;
-    vpeso = parseFloat(peso);
-    valtura = parseFloat(altura);
-    vidade = parseFloat(idade);
-
-
-
-    if (genero == 'masculino'){
-        vresultado = parseFloat(66 + (13.7 * vpeso) + (5 * valtura) - (6.8 * vidade));
-        document.getElementById("result_1").innerHTML = vresultado.toFixed(2);
-        var total = vresultado + (vresultado * 0.25);
-        vnenhuma = parseFloat(total); 
-        document.getElementById("result_2").innerHTML = vnenhuma.toFixed(2);
-        
-        total = vresultado + (vresultado * 0.35);
-        vmoderada = parseFloat(total); 
-        document.getElementById("result_3").innerHTML = vmoderada.toFixed(2);
-        total = vresultado + (vresultado * 0.45);
-        vintensa = parseFloat(total); 
-        document.getElementById("result_4").innerHTML = vintensa.toFixed(2);        
-     }else{
-        vresultado = parseFloat(655 + (9.6 * vpeso) + (1.7 * valtura) - (4.7 * vidade));
-        document.getElementById("result_1").innerHTML = vresultado.toFixed(2);
-        var total = vresultado + (vresultado * 0.20);
-        vnenhuma = parseFloat(total); 
-        document.getElementById("result_2").innerHTML = vnenhuma.toFixed(2);   
-        
-           total = vresultado + (vresultado * 0.30);
-        vmoderada = parseFloat(total); 
-        document.getElementById("result_3").innerHTML = vmoderada.toFixed(2);
-        
-        total = vresultado + (vresultado * 0.40);
-        vintensa = parseFloat(total); 
-        document.getElementById("result_4").innerHTML = vintensa.toFixed(2);             
-     } 
-
-    return [imc.toFixed(2), classificacao]
+    if(getPeso() == '' || getPeso() < 0){
+        alert('Peso invalido');
+        return false;
+    }
+    return true;
 }
+
+
+function clearInputs() {
+    $('#idade').val('');
+    $('#altura').val('');
+    $('#peso').val('');
+}
+
+function getIMC() {
+    var peso = getPeso();
+    var altura = getAltura() / 100;
+    var imc = peso / (altura * altura);
+    return imc.toFixed(5);
+}
+
+function getATV() {
+    var Atv = $('input[name="get"]:checked').val();
+    return Atv;
+}
+
+function getData() {
+    console.log(
+        'Altura: ' + getAltura() + '\n' +
+        'Peso: ' + getPeso() + '\n' +
+        'Idade: ' + getIdade() + '\n' +
+        'Genero: ' + getGender() + '\n' +
+        'Nivel de Atividade: ' + getATV() + '\n' +
+        'IMC: ' + getIMC() + '\n' +
+        'AF: ' + getAF()
+    );
+}
+
+function getAF() {
+    var genero = getGender();
+    var imc = getIMC();
+    var nivelAtv = getATV();
+    var afValues = {
+        '1': { 'masculino': 1.0, 'feminino': 1.0 },
+        '2': { 'masculino': imc < 25 ? 1.11 : 1.12, 'feminino': 1.12 },
+        '3': { 'masculino': imc < 25 ? 1.25 : 1.29, 'feminino': 1.27 },
+        '4': { 'masculino': imc < 25 ? 1.48 : 1.44, 'feminino': 1.45 }
+    };
+    return afValues[nivelAtv][genero];
+}
+
+function ImcClasify() {
+    var imc = getIMC();
+    var genero = getGender();
+    var imcClassify = {
+        'masculino': {
+            'Abaixo do peso (muito grave)': imc < 16,
+            'Abaixo do peso (grave)': imc >= 16 && imc < 16.99,
+            'Abaixo peso': imc >= 17 && imc < 18.49,
+            'Peso normal': imc >= 18.5 && imc < 24.99,
+            'Sobrepeso': imc >= 25 && imc < 29.99,
+            'Obesidade grau I': imc >= 30 && imc < 34.99,
+            'Obesidade grau II': imc >= 35 && imc < 39.99,
+            'Obesidade grau III': imc >= 40
+        },
+        'feminino': {
+            'Abaixo peso muito grave': imc < 16,
+            'Abaixo peso grave': imc >= 16 && imc < 16.99,
+            'Abaixo peso': imc >= 17 && imc < 18.49,
+            'Peso normal': imc >= 18.5 && imc < 24.99,
+            'Sobrepeso': imc >= 25 && imc < 29.99,
+            'Obesidade grau I': imc >= 30 && imc < 34.99,
+            'Obesidade grau II': imc >= 35 && imc < 39.99,
+            'Obesidade grau III': imc >= 40
+        }
+    };
+
+    var classify = imcClassify[genero];
+    for (var key in classify) {
+        if (classify[key]) {
+            return key;
+        }
+    }
+}
+
+function getAltura() {
+    return $('#altura').val();
+}
+
+function getEstaturaMetros() {
+    return getAltura() / 100;
+}
+
+function getIdade() {
+    return $('#idade').val();
+}
+
+function getPeso() {
+    return $('#peso').val();
+}
+
+function getGender() {
+    var genero = $('#sex1').is(':checked') ? 'feminino' : 'masculino';
+    return genero;
+}
+
+function getBaseValue() {
+    var genero = getGender();
+    var imc = getIMC();
+    var baseValues = {
+        'masculino': imc < 25 ? 662 : 1086,
+        'feminino': imc < 25 ? 354 : 448
+    };
+    return baseValues[genero];
+}
+
+function getSubtractBaseValue() {
+    var genero = getGender();
+    var imc = getIMC();
+    var subtractBaseValues = {
+        'masculino': imc < 25 ? 9.53 : 10.1,
+        'feminino': imc < 25 ? 6.91 : 7.95
+    };
+    return subtractBaseValues[genero];
+}
+
+function getMathValue1() {
+    var genero = getGender();
+    var imc = getIMC();
+    var mathValues1 = {
+        'masculino': imc < 25 ? 15.91 : 13.7,
+        'feminino': imc < 25 ? 9.36 : 11.4
+    };
+    return mathValues1[genero];
+}
+
+function getMathValue2() {
+    var genero = getGender();
+    var imc = getIMC();
+    var mathValues2 = {
+        'masculino': imc < 25 ? 539.5 : 416,
+        'feminino': imc < 25 ? 726 : 619
+    };
+    return mathValues2[genero];
+}
+
+function getGET() {
+    var get = getBaseValue() - (getSubtractBaseValue() * getIdade()) + (getAF() * (getMathValue1() * getPeso() + getMathValue2() * getEstaturaMetros()));
+    return get.toFixed(2);
+}
+
